44b0b93d4f9c4deced660efe33097f43e8902dc9
Frans J Titulaer <fransjlt@gmail.com>
2023-02-10 13:50:42 +0100

    firebaseConfig added

---
+++ src/lib/firebase.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBx9TiREYWSu9-dTZxbiylJbdXhFYvDLt4",
+  authDomain: "simplewish-ba6d8.firebaseapp.com",
+  projectId: "simplewish-ba6d8",
+  storageBucket: "simplewish-ba6d8.appspot.com",
+  messagingSenderId: "305875419660",
+  appId: "1:305875419660:web:de6a3e385740a66c5ee429",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
